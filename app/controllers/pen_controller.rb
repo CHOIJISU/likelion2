b963053a8698421183d859ef4ec50ff70318d927
CHOIJISU <nidnot2know@naver.com>
2017-03-28 11:42:21 +0000

    random menu

---
+++ app/controllers/pen_controller.rb
@@ -0,0 +1,13 @@
+class PenController < ApplicationController
+ def index
+     @down="안녕하세요"
+   @mango=["짜장면","탕수육"]
+   @pen=@mango.sample 
+   @my_image=@pen+".jpg"
+ 
+ 
+ 
+ end
+ def peeen
+ end
+end
